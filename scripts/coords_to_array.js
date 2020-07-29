3d373be92e8d7d53e292d58f8357c89c920de0e2
losslessring <45686453+losslessring@users.noreply.github.com>
2020-07-30 00:35:41 +0300

    Add files via upload

--- scripts/coords_to_array.js
+++ scripts/coords_to_array.js
@@ -26,13 +26,45 @@ const range_to_array = (start, end, steps) => {
 
 
 
-const coords_to_points_array = (line, step) => {
+const coords_to_points_array = (line, steps) => {
 	
-	let x = range_to_array(line.beginPoint.x, line.endPoint.x, step)
-	let y = range_to_array(line.beginPoint.y, line.endPoint.y, step)
-	let z = range_to_array(line.beginPoint.z, line.endPoint.z, step)
+	let x = range_to_array(line.beginPoint.x, line.endPoint.x, steps)
+	let y = range_to_array(line.beginPoint.y, line.endPoint.y, steps)
+	let z = range_to_array(line.beginPoint.z, line.endPoint.z, steps)
 
 	return {x, y, z}
 }
 
-export {range_to_array, coords_to_points_array}
+const ends_to_points = (line, steps) => {
+	
+	let x_array = range_to_array(line.beginPoint.x, line.endPoint.x, steps)
+	let y_array = range_to_array(line.beginPoint.y, line.endPoint.y, steps)
+	let z_array = range_to_array(line.beginPoint.z, line.endPoint.z, steps)
+
+	let points = []
+
+	for (let i = 0; i < steps; i++){
+		let coords = {x: x_array[i], y: y_array[i], z: z_array[i]}
+		points.push(coords)
+	}
+
+	return points
+}
+
+
+const check_intersection = (array1, array2, limit) => {
+	for (let i = 0; i < array1.length; i++){
+		for (let j = 0; j < array1.length; j++){
+			if (Math.abs(array1[i].x - array2[j].x) <= limit &&
+				Math.abs(array1[i].y - array2[j].y) <= limit && 
+				Math.abs(array1[i].z - array2[j].z) <= limit) {
+					
+					return {array1 :array1[i], array2: array2[j]}
+			}
+		}
+	}
+	return false
+}
+
+
+export {range_to_array, coords_to_points_array, ends_to_points, check_intersection}
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
 import {geometry_objects} from './geometry.js'
 
-import {range_to_array, coords_to_points_array} from './coords_to_array.js'
+import {range_to_array, coords_to_points_array, ends_to_points, check_intersection} from './coords_to_array.js'
 
 import {draw} from './draw.js'
 
 
 //console.log(range_to_array(1, 1, 10))
-geometry_objects.forEach(geometry_object => console.log(coords_to_points_array(geometry_object, 10)))
+//geometry_objects.forEach(geometry_object => console.log(coords_to_points_array(geometry_object, 10)))
 
+//geometry_objects.forEach(geometry_object => console.log(ends_to_points(geometry_object, 10)))
+
+//Перевод линий в массив точек для расчета пересечения. Подумать, куда это запихнуть
+
+let lines_points = geometry_objects.map(geometry_object => ends_to_points(geometry_object, 10))
+console.log(lines_points)
+
+// Расчет пересечения
+console.log(check_intersection(lines_points[0], lines_points[1], 0.5))
 //console.log(geometry_objects[0])
-draw(geometry_objects[0])
+//draw(geometry_objects[0])
